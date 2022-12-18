e5792a0c5b7ae382ca1644aee8ea70bd512b994e
Kian2002 <canadakurdistan@gmail.com>
2022-12-17 16:34:39 -0800

    added DB & updated packages

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require('lodash');
-const posts = [];
+const _ = require("lodash");
+const mongoose = require("mongoose");
 
-const homeStartingContent =
-  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const ATLAS = process.env.ATLAS;
+
+mongoose.connect("mongodb+srv://zuptex:U6LbrHEqtFawmowd@cluster0.qpnemz1.mongodb.net/blogDB");
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  body: String,
+});
+
+const Posts = mongoose.model("Posts", postSchema);
+
 const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
@@ -19,7 +28,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home", { homeStartingContent: homeStartingContent, posts: posts});
+  Posts.find({}, (err, posts) => {
+    if (!err) {
+      res.render("home", { posts: posts });
+    }
+  });
 });
 
 app.get("/about", (req, res) => {
@@ -35,25 +48,33 @@ app.get("/compose", (req, res) => {
 });
 
 app.post("/compose", (req, res) => {
-  const newPost = { title: req.body.title, body: req.body.body };
-  posts.push(newPost);
-  res.redirect("/");
-});
-
-app.get("/posts/:postTitle", (req, res) =>{
-
-  const reqTitle = _.lowerCase(req.params.postTitle);
-
-  posts.forEach((post) => {
-    const storedTitle = _.lowerCase(post.title);
-    const postBody = post.body;
-
-    if (storedTitle === reqTitle){
-      res.render("post", {storedTitle: storedTitle, postBody: postBody})
-    } 
+  const post = new Posts({ title: req.body.title, body: req.body.body });
+  post.save((err) => {
+    if (!err) {
+      res.redirect("/");
+    }
   });
 });
 
-app.listen(3000, function () {
+app.get("/posts/:postTitle", (req, res) => {
+  const reqTitle = _.lowerCase(req.params.postTitle);
+
+  Posts.find({}, (err, posts) => {
+    if (!err) {
+      posts.forEach((post) => {
+        const storedTitle = _.lowerCase(post.title);
+        const postBody = post.body;
+
+        if (storedTitle === reqTitle) {
+          res.render("post", { storedTitle: storedTitle, postBody: postBody });
+        }
+      });
+    }
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
   console.log("Server started on port 3000");
 });
